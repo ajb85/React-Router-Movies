2d397541ad7bc6a2b27bcdead8f2a82d2f8a42c3
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-07 21:27:14 -0400

    Converts to hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,26 @@
-import React, { Component } from "react";
-
+import React, { useState } from "react";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import { Route } from "react-router-dom";
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedList: []
-    };
-  }
+function App() {
+  const [savedList, setSavedList] = useState([]);
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
+  const addToSavedList = movie => {
+    setSavedList([...savedList, movie]);
   };
 
-  render() {
-    return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <Route path="/" exact component={MovieList} />
-        <Route
-          path="/movies/:id"
-          render={props => (
-            <Movie {...props} addToSavedList={this.addToSavedList} />
-          )}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <SavedList list={savedList} />
+      <Route path="/" exact component={MovieList} />
+      <Route
+        path="/movies/:id"
+        render={props => <Movie {...props} addToSavedList={addToSavedList} />}
+      />
+    </div>
+  );
 }
+
+export default App;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,59 +1,27 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard.js";
 
-export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
+function MovieList(props) {
+  const [movies, setMovies] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(response => {
-        this.setState(() => ({ movies: response.data }));
-      })
-      .catch(error => {
-        console.error("Server Error", error);
-      });
-  }
+      .then(res => setMovies(res.data))
+      .catch(error => console.error("Server Error", error));
+  }, [setMovies]);
 
-  render() {
-    return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
-            <MovieCard key={movie.id} movie={movie} />
-          </Link>
-        ))}
-      </div>
-    );
-  }
-}
-
-// Obsolete
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
+    <div className="movie-list">
+      {movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
+          <MovieCard key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
+
+export default MovieList;
